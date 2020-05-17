e776ef52ff85177efafdd9be7c19370f8c417fcd
lilei <random2035@qq.com>
2020-05-17 19:15:33 +0800

    x

--- release/install.sh
+++ release/install.sh
@@ -1,40 +1,91 @@
 #!/usr/bin/env bash
 
-now=$(pwd)
+INSTALL_DIR=$HOME
 
 which git > /dev/null || apt-get install git -y
 
+# install golang
 if [[ -z "$GOPATH" ]]; then
   sh install_go.sh
   mkdir /v2ray &> /dev/null
   export GOPATH=/v2ray
 fi
 
-mkdir -p '/etc/v2ray' '/var/log/v2ray' '/usr/bin/v2ray'
-
 PJ_NAME="v2ray-core"
+pj_dir=$HOME/${PJ_NAME}
 
-if [[ ! -e ${PJ_NAME} ]];then
-    PROJECT_URL="https://github.com/chaseSpace/$PJ_NAME.git"
-    git clone ${PROJECT_URL}
-fi
+dir_etc_v2='/etc/v2ray'
+dir_log_v2='/var/log/v2ray'
+dir_usrbin_v2='/usr/bin/v2ray'
 
-pj_dir=$(pwd)/${PJ_NAME}
+use_json_cfg='/etc/v2ray/config.json'
 
-cd ${pj_dir}/main && go build -o $GOPATH/bin/v2ray
-cd ${pj_dir}/infra/control/main &&  go build -o $GOPATH/bin/v2ctl
+__copy_config(){
+    force=$1
+    if [[ ${force} = '-f' ]];then
+        /bin/cp ${pj_dir}/main/config.json ${use_json_cfg}
+    else
+        if [[ ! -e ${use_json_cfg} ]];then
+            cp ${pj_dir}/main/config.json ${use_json_cfg}
+        fi
+    fi
+}
 
-cd ${now}
+__compile_and_overwrite_old(){
+    git pull origin
+    cd ${pj_dir}/main && go build -o $GOPATH/bin/v2ray
+    cd ${pj_dir}/infra/control/main &&  go build -o $GOPATH/bin/v2ctl
 
-/bin/cp -rf $GOPATH/bin/v2ray /usr/bin/v2ray/
-/bin/cp -rf $GOPATH/bin/v2ctl /usr/bin/v2ray/
+    cd ${INSTALL_DIR}
 
-/bin/cp -rf ${pj_dir}/release/config/systemd/v2ray.service /etc/systemd/system
+    /bin/cp -rf $GOPATH/bin/v2ray $dir_usrbin_v2/
+    /bin/cp -rf $GOPATH/bin/v2ctl $dir_usrbin_v2/
+    /bin/cp -rf ${pj_dir}/release/config/*.* $dir_usrbin_v2/
 
-jsonCfg='/etc/v2ray/config.json'
+    /bin/cp -rf ${pj_dir}/release/config/systemd/v2ray.service /etc/systemd/system
+}
 
-if [ ! -e $jsonCfg ];then
-    cp ${pj_dir}/main/config.json ${jsonCfg}
-fi
+install (){
+    mkdir -p $dir_etc_v2 $dir_log_v2 $dir_usrbin_v2
 
-syste mctl enable v2ray.service
+    if [[ ! -e ${PJ_NAME} ]];then
+        PROJECT_URL="https://github.com/chaseSpace/$PJ_NAME.git"
+        git clone ${PROJECT_URL}
+    fi
+
+    __compile_and_overwrite_old
+
+    # overwrite if file exists
+    __copy_config -f
+
+    export V2RAY_LOCATION_TOOL=$dir_usrbin_v2
+    export V2RAY_LOCATION_ASSET=$dir_usrbin_v2
+
+    systemctl restart v2ray.service
+}
+
+
+update(){
+    cd ${pj_dir}
+
+    __compile_and_overwrite_old
+
+    # it will cancel if file exists(not force)
+    __copy_config
+
+    systemctl restart v2ray.service
+}
+
+main(){
+    arg=$1
+    case $arg in
+    '-i') echo 'start install...'
+        install ;;
+    '-u') echo 'start update...'
+        update ;;
+    *) echo 'You must input args what you want!'
+        ;;
+    esac
+}
+
+main
